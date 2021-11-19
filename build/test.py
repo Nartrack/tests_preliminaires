a26184f30ac5896276ef309248f92e9064c2b364
Nartrack <gilleschorter@gmail.com>
2021-11-19 17:59:47 +0100

    ajout des getters et setters

--- build/test.py
+++ build/test.py
@@ -3,6 +3,7 @@ import main as m
 hello_objet = m.hello("Gilles")
 hello_objet.hello_nom()
 print("")
-
+hello_objet.nom = "Hugues"
+print(hello_objet.nom + "\n")
 hello_plus_objet = m.hello_plus("Louis")
 hello_plus_objet.hello_nom()
