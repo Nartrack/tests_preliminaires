f6ba6ed99b45c56dcd566fee9cf9c03a9b3ab52d
Nartrack <gilleschorter@gmail.com>
2021-11-20 20:46:58 +0100

    réglage du problème d’ordre d’affichage en console

--- build/test.py
+++ build/test.py
@@ -6,12 +6,10 @@ print(fam1.nom + "\n")
 fam1.nom = "Schorter"
 fam1.hello_nom()
 print("")
+
 fam2 = m.famille_part("de", "Montbron")
 fam2.hello_nom()
-print("ok\n")
-fam1.hello_nom()
+print("\n")
+
 moi = m.personne("Gilles", fam1)
 moi.bonjour()
-
-
-print("ok2")
